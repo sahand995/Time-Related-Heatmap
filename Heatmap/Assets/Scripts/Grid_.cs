6bad3d1e0911a8ec6c21c39cdf2a348e3b4e1314
Sahand Fathi <s.fathi995@gmail.com>
2021-10-26 12:44:20 +0330

    Heatmap

---
+++ Heatmap/Assets/Scripts/Grid_.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CodeMonkey.Utils;
+public class Grid_ 
+{
+    public const int HEAT_MAP_MAX_VALUE = 200;
+    public const int HEAT_MAP_MIN_VALUE = 0;
+
+    public event EventHandler<OnGridValueChangedEventArgs> OnGridValueChanged;
+    public class OnGridValueChangedEventArgs : EventArgs
+    {
+        public int x;
+        public int y;
+    }
+
+    private int width;
+    private int height;
+    private float cellSize;
+    private TextMesh[,] debugTextArray;
+    private Vector3 originPosition;
+
+    private int[,] gridArray;
+
+    public Grid_(int width, int height, float cellSize, Vector3 originPosition)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+        this.originPosition = originPosition;
+
+        gridArray = new int[width, height];
+        debugTextArray = new TextMesh[width, height];
+
+        for (int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(1); y++)
+            {
+                debugTextArray[x, y] = UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize, 0, cellSize) * 0.5f,
+                                      1, Color.white, TextAnchor.MiddleCenter);
+                debugTextArray[x, y].transform.rotation = Quaternion.LookRotation(new Vector3(0, 90, 0));
+
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+
+        OnGridValueChanged += (object sender, OnGridValueChangedEventArgs eventArgs) => 
+        {
+            debugTextArray[eventArgs.x, eventArgs.y].text = gridArray[eventArgs.x, eventArgs.y].ToString();
+        };
+    }
+    public int GetWidth()
+    {
+        return width;
+    }
+
+    public int GetHeight()
+    {
+        return height;
+    }
+
+    public float GetCellSize()
+    {
+        return cellSize;
+    }
+    public Vector3 GetWorldPosition(int x, int y)
+    {
+        return new Vector3(x, 0, y) * cellSize + originPosition;
+    }
+
+    private void GetXY(Vector3 worldPosition, out int x, out int y)
+    {
+        x = Mathf.FloorToInt(worldPosition.x - originPosition.x / cellSize);
+        y = Mathf.FloorToInt(worldPosition.y - originPosition.y / cellSize);
+    }
+    public void SetValue(int x, int y, int value)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            gridArray[x, y] = Mathf.Clamp(value, HEAT_MAP_MIN_VALUE, HEAT_MAP_MAX_VALUE);
+            //debugTextArray[x, y].text = gridArray[x, y].ToString();
+            if (OnGridValueChanged != null) OnGridValueChanged(this, new OnGridValueChangedEventArgs { x = x, y = y });
+        }
+    }
+
+    public void SetValue(Vector3 worldPosition, int value)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        SetValue(x, y, value);
+    }
+
+    public int GetValue(int x, int y)
+    {
+        if (x >= 0 && y >= 0 && x < width && y < height)
+        {
+            return gridArray[x, y];
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public int GetValue(Vector3 worldPosition)
+    {
+        int x, y;
+        GetXY(worldPosition, out x, out y);
+        return GetValue(x, y);
+    }
+
+    public void AddValue(int x, int y, int value)
+    {
+        SetValue(x, y, GetValue(x, y) + value);
+    }
+
+
+}
+
---
+++ Heatmap/Assets/Scripts/HeatMapVisual.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatMapVisual : MonoBehaviour
+{
+    private Grid_ grid;
+    private Mesh mesh;
+
+    private void Awake()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+    }
+    public void SetGrid(Grid_ grid)
+    {
+        this.grid = grid;
+        UpdateHeatMapVisual();
+
+        grid.OnGridValueChanged += grid_OnGridValueChanged;
+    }
+
+    private void grid_OnGridValueChanged(object sender, Grid_.OnGridValueChangedEventArgs e)
+    {
+        UpdateHeatMapVisual();
+    }
+
+    private void UpdateHeatMapVisual()
+    {
+        MeshUtils.CreateEmptyMeshArrays(grid.GetWidth() * grid.GetHeight(), out Vector3[] vertices, out Vector2[] uv, out int[] triangles);
+
+        for (int x = 0; x < grid.GetWidth(); x++)
+        {
+            for (int y = 0; y < grid.GetHeight(); y++)
+            {
+                int index = x * grid.GetHeight() + y;
+                Vector3 quadSize = new Vector3(1, 1) * grid.GetCellSize();
+
+                int gridValue = grid.GetValue(x, y);
+                float gridValueNormalized = (float)gridValue / Grid_.HEAT_MAP_MAX_VALUE;
+                Vector2 gridValueUV = new Vector2(gridValueNormalized, 0f);
+
+
+
+                MeshUtils.AddToMeshArrays(vertices, uv, triangles, index, new Vector3(grid.GetWorldPosition(x, y).x, grid.GetWorldPosition(x, y).z, 0) + quadSize * .5f, 
+                    0f, quadSize, gridValueUV, gridValueUV);
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+    }
+
+}
+
